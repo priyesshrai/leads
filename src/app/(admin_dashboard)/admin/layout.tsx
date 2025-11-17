f0675b5557be7b4fb40b0272e804f5ed549a370a
priyesshrai <priyeshrai369@gmail.com>
2025-11-17 16:58:48 +0530

    update admin dashboard

--- src/app/(admin_dashboard)/admin/layout.tsx
+++ src/app/(admin_dashboard)/admin/layout.tsx
@@ -1,11 +1,13 @@
 import SideBar from "@/src/components/common/SideBar";
+import TopBar from "@/src/components/common/TopBar";
 import { ADMIN_TABS } from "@/src/constants/adminTabs";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <main className="flex min-h-screen">
-            <SideBar tabsItems={ADMIN_TABS} />
-            <section className="flex-1 ml-[16%] p-6">
+            <SideBar />
+            <section className="flex-1 ml-[16.7%] bg-blue-50">
+                <TopBar />
                 {children}
             </section>
         </main>
